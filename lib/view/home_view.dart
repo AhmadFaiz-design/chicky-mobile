a30d8df98fc3bed3a33a10e523f7214e77b1c51d
AhmadFaiz-Design <ahmafaiz007@gmail.com>
2026-06-07 00:43:16 +0800

    feat(home): menambahkan row monitoring (ayam, pakan, telur)

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -14,8 +14,11 @@ class _HeaderCard extends StatelessWidget {
           Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text("Halo,", style: TextStyle(fontSize: 14),),
-              Text("Ahmad Faiz", style: TextStyle(fontWeight: FontWeight.w500, fontSize: 16),)
+              Text("Halo,", style: TextStyle(fontSize: 14)),
+              Text(
+                "Ahmad Faiz",
+                style: TextStyle(fontWeight: FontWeight.w500, fontSize: 16),
+              ),
             ],
           ),
           Container(
@@ -23,14 +26,17 @@ class _HeaderCard extends StatelessWidget {
             height: 30,
             decoration: BoxDecoration(
               shape: BoxShape.circle,
-              color: Colours.mainOrange
+              color: Colours.mainOrange,
             ),
             child: IconButton(
-              onPressed: (){}, 
-              icon: SvgPicture.asset(IconsSVG.notification, colorFilter: ColorFilter.mode(Colors.white, BlendMode.srcIn),),
+              onPressed: () {},
+              icon: SvgPicture.asset(
+                IconsSVG.notification,
+                colorFilter: ColorFilter.mode(Colors.white, BlendMode.srcIn),
+              ),
               iconSize: 10,
               padding: EdgeInsets.all(6),
-            )
+            ),
           ),
         ],
       ),
@@ -38,6 +44,76 @@ class _HeaderCard extends StatelessWidget {
   }
 }
 
+class _Dashboard extends StatelessWidget {
+  Widget _cardRow({
+    required String icon,
+    required String name,
+    required int sum,
+  }) {
+    return Expanded(
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 8),
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.black),
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colours.mainOrange, width: 2),
+                    borderRadius: BorderRadius.circular(12)
+                  ),
+                  height: 34,
+                  width: 34,
+                  child: Padding(
+                    padding: EdgeInsets.all(5),
+                    child: SvgPicture.asset(
+                      icon,
+                      colorFilter: ColorFilter.mode(
+                        Colours.mainOrange,
+                        BlendMode.srcIn,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 4),
+                Text(name, style: TextStyle(fontSize: 14))
+              ],
+            ),
+            SizedBox(height: 6),
+            Text(sum.toString(), style: TextStyle(fontWeight: FontWeight.w700, fontSize: 20))
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _rowMonitoring() {
+    return Row(
+      children: [
+        _cardRow(icon: IconsSVG.chicken, name: "Ayam", sum: 200),
+        SizedBox(width: 8),
+        _cardRow(icon: IconsSVG.beans, name: "Pakan", sum: 175),
+        SizedBox(width: 8),
+        _cardRow(icon: IconsSVG.egg, name: "Telur", sum: 2000)
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        _rowMonitoring()
+      ]
+    );
+  }
+}
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -55,11 +131,12 @@ class HomeState extends State<HomeScreen> {
           padding: EdgeInsets.symmetric(horizontal: 20),
           child: Column(
             children: [
-              _HeaderCard()
-            ],
+              _HeaderCard(),
+              _Dashboard()
+            ]
           ),
         ),
-      )
+      ),
     );
   }
 }
