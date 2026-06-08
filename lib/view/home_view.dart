921b61605216584e81feae95e83ecd71ee3cfb1b
AhmadFaiz-Design <ahmafaiz007@gmail.com>
2026-06-08 13:44:01 +0800

    feat(recent-card): menambahkan fitur fcr terbaru di home_view

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -1,7 +1,9 @@
 import 'package:chicky/core/icons.dart';
+import 'package:chicky/widgets/test.dart';
 import 'package:flutter/material.dart';
 import '../core/colours.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import '../widgets/performance_chart_card.dart';
 
 class _HeaderCard extends StatelessWidget {
   @override
@@ -66,7 +68,7 @@ class _Dashboard extends StatelessWidget {
                 Container(
                   decoration: BoxDecoration(
                     border: Border.all(color: Colours.mainOrange, width: 2),
-                    borderRadius: BorderRadius.circular(12)
+                    borderRadius: BorderRadius.circular(12),
                   ),
                   height: 34,
                   width: 34,
@@ -82,11 +84,14 @@ class _Dashboard extends StatelessWidget {
                   ),
                 ),
                 SizedBox(width: 4),
-                Text(name, style: TextStyle(fontSize: 14))
+                Text(name, style: TextStyle(fontSize: 14)),
               ],
             ),
             SizedBox(height: 6),
-            Text(sum.toString(), style: TextStyle(fontWeight: FontWeight.w700, fontSize: 20))
+            Text(
+              sum.toString(),
+              style: TextStyle(fontWeight: FontWeight.w700, fontSize: 20),
+            ),
           ],
         ),
       ),
@@ -94,23 +99,146 @@ class _Dashboard extends StatelessWidget {
   }
 
   Widget _rowMonitoring() {
-    return Row(
-      children: [
-        _cardRow(icon: IconsSVG.chicken, name: "Ayam", sum: 200),
-        SizedBox(width: 8),
-        _cardRow(icon: IconsSVG.beans, name: "Pakan", sum: 175),
-        SizedBox(width: 8),
-        _cardRow(icon: IconsSVG.egg, name: "Telur", sum: 2000)
-      ],
+    return Container(
+      margin: EdgeInsets.only(bottom: 20),
+      child: Row(
+        children: [
+          _cardRow(icon: IconsSVG.chicken, name: "Ayam", sum: 200),
+          SizedBox(width: 8),
+          _cardRow(icon: IconsSVG.beans, name: "Pakan", sum: 175),
+          SizedBox(width: 8),
+          _cardRow(icon: IconsSVG.egg, name: "Telur", sum: 2000),
+        ],
+      ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return Column(children: [_rowMonitoring()]);
+  }
+}
+
+class _BannerCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
       children: [
-        _rowMonitoring()
-      ]
+        Container(
+          margin: EdgeInsets.only(bottom: 20),
+          padding: EdgeInsets.all(14),
+          width: double.infinity,
+          height: 76,
+          decoration: BoxDecoration(
+            color: Colours.mainOrange,
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Unlock your",
+                style: TextStyle(fontSize: 16, color: Colors.white),
+              ),
+              Text(
+                "chicken's potential!",
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Positioned(
+          bottom: 12,
+          right: -4,
+          child: Container(
+            height: 86,
+            width: 86,
+            child: SvgPicture.asset(
+              IconsSVG.chicken,
+              colorFilter: ColorFilter.mode(Colors.white, BlendMode.srcIn),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _recentHistoryLog extends StatelessWidget {
+
+  List<double> dataFCR = [1.1, 1.2, 0.8];
+
+  DateTime now = DateTime.now();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(20),
+      margin: EdgeInsets.only(bottom: 20),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        border: Border.all(color: Colors.black),
+      ),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "FCR Terbaru",
+                style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
+              ),
+              InkWell(
+                onTap: () {},
+                child: Text(
+                  "See all",
+                  style: TextStyle(color: Colours.mainOrange, fontSize: 14, fontWeight: FontWeight.w500),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 10),
+          ListView.builder(
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            itemCount: dataFCR.length,
+            itemBuilder: (context, index) {
+              return Container(
+                margin: EdgeInsets.only(bottom: 10),
+                padding: EdgeInsets.all(12),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black),
+                  borderRadius: BorderRadius.circular(12)
+                ),
+                width: double.infinity,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("${dataFCR[index].toString()} Kg", style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500)),
+                        Text("low to 1%", style: TextStyle(color: Colours.green, fontSize: 12,))
+                      ],
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Kamis", style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500)),
+                        Text("21-12-2012", style: TextStyle( fontSize: 12,))
+                      ],
+                    )
+                  ],
+                ),
+              );
+            }
+          )
+        ],
+      ),
     );
   }
 }
@@ -127,13 +255,15 @@ class HomeState extends State<HomeScreen> {
       backgroundColor: Colors.white,
       body: SafeArea(
         child: SingleChildScrollView(
-          physics: NeverScrollableScrollPhysics(),
           padding: EdgeInsets.symmetric(horizontal: 20),
           child: Column(
             children: [
               _HeaderCard(),
-              _Dashboard()
-            ]
+              FcrDesemberChart(),
+              _Dashboard(),
+              _BannerCard(),
+              _recentHistoryLog(),
+            ],
           ),
         ),
       ),
