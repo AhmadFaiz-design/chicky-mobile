823578c49a2ff066f2bbde295a587760e336bb0c
AhmadFaiz-Design <ahmafaiz007@gmail.com>
2026-06-26 13:55:26 +0800

    feat(treshold input): memberikan batasan wajar setiap data input

--- lib/controllers/fcr_controller.dart
+++ lib/controllers/fcr_controller.dart
@@ -2,31 +2,98 @@ import 'package:chicky/models/fcr_model.dart';
 
 class FCRController {
 
-  double calculatedFCR(FCRModel inputData){
-    const double intercept = -1.6231;
-    const double wChicken = 1.14;   // Menurunkan efisiensi jika terlalu padat
-    const double wFeeding = 1.23;   // Pengaruh pakan masuk berlebih
-    const double wAmmonia = -1.05;  // Pengaruh negatif korelasi amonia
-    const double wTemp = -0.62;     // Pengaruh suhu optimal
-    const double wHumidity = 1.02;  // Pengaruh kelembaban tinggi
-    const double wLight = -0.26;    // Pengaruh intensitas cahaya
-    const double wNoise = 0.57;     // Faktor stres akibat kebisingan
+  List<String> getValidation({required wrongInputModel validationResult}){
+    List<String> listWrongInput = [];
 
-    double FCRResult = intercept
-    + (wChicken * inputData.ayam)
-    + (wFeeding + inputData.pakan)
-    + (wAmmonia + inputData.amonia)
-    + (wTemp + inputData.suhu)
-    + (wHumidity + inputData.kelembaban)
-    + (wLight + inputData.cahaya)
-    + (wNoise + inputData.bising);
-
-    if (FCRResult < 0.0){
-      FCRResult = 0.0;
+    if(validationResult.pakan != null){
+      listWrongInput.add("Jumlah Pakan");
+    }
+    if(validationResult.amonia != null){
+      listWrongInput.add("Gas Amonia");
+    }
+    if(validationResult.suhu != null){
+      listWrongInput.add("Suhu");
+    }
+    if(validationResult.kelembaban != null){
+      listWrongInput.add("Kelembaban");
+    }
+    if(validationResult.cahaya != null){
+      listWrongInput.add("Intensitas Cahaya");
+    }
+    if(validationResult.bising != null){
+      listWrongInput.add("Kebisingan Kandang");
     }
 
-    inputData.hasilFCR = FCRResult;
+    return listWrongInput;
+  }
 
-    return FCRResult;
+  wrongInputModel validationCheck({
+    required FCRModel inputData,
+    required wrongInputModel wrongInput,
+  }) {
+    if (inputData.ayam <= 10) {
+      if (inputData.pakan < 1.1) {
+        wrongInput.pakan = inputData.pakan;
+      }
+    }
+    if (inputData.ayam > 10) {
+      if (inputData.pakan < 1.2) {
+        wrongInput.pakan = inputData.pakan;
+      }
+    }
+    if (inputData.amonia < 0.0 || inputData.amonia > 30.0) {
+      wrongInput.amonia = inputData.amonia;
+    }
+    if (inputData.suhu < 15.0 || inputData.suhu > 40.0) {
+      wrongInput.suhu = inputData.suhu;
+    }
+    if (inputData.kelembaban < 40.0 || inputData.kelembaban > 90.0) {
+      wrongInput.kelembaban = inputData.kelembaban;
+    }
+    if (inputData.cahaya < 0.0 || inputData.cahaya > 50.0) {
+      wrongInput.cahaya = inputData.cahaya;
+    }
+    if (inputData.bising < 30.0 || inputData.bising > 90.0) {
+      wrongInput.bising = inputData.bising;
+    }
+    return wrongInput;
+  }
+
+  double calculatedFCR({
+    required FCRModel inputData,
+  }) {
+    const double intercept = -2.5;
+    const double wChicken = 0.60; // Menurunkan efisiensi jika terlalu padat
+    const double wFeeding = 2.0; // Pengaruh pakan masuk berlebih
+    const double wAmmonia = -0.20; // Pengaruh negatif korelasi amonia
+    const double wTemp = -0.10; // Pengaruh suhu optimal
+    const double wHumidity = 0.05; // Pengaruh kelembaban tinggi
+    const double wLight = 0.20; // Pengaruh intensitas cahaya
+    const double wNoise = -0.05; // Faktor stres akibat kebisingan
+
+    double eggProductionResult =
+        intercept +
+        (wChicken * inputData.ayam) +
+        (wFeeding * inputData.pakan) +
+        (wAmmonia * inputData.amonia) +
+        (wTemp * inputData.suhu) +
+        (wHumidity * inputData.kelembaban) +
+        (wLight * inputData.cahaya) +
+        (wNoise * inputData.bising);
+
+    // Mencegah hasil minus jika ayam stres berat
+    if (eggProductionResult < 0) return 0.0;
+
+    print('Jumlah prediksi telur: $eggProductionResult');
+
+    // Rata-rata berat 1 butir telur ayam ras adalah 60 gram (0.06 Kg)
+    double eggWeight = eggProductionResult * 0.06;
+    double resultFCR = inputData.pakan / eggWeight;
+    inputData.hasilFCR = resultFCR;
+
+    print('Total Berat Telur:  $eggWeight');
+    print('Hasil FCR:  $resultFCR');
+
+    return resultFCR;
   }
 }
